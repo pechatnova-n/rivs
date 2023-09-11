2da20d3d9d53227ba2e82359789851a4c88db5e2
DESKTOP-FR5AHN7\knyaz <knyazew.sv@gmail.com>
2023-09-11 11:34:50 +0300

    1

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,8 @@
 import '../styles/style.scss';
 import 'virtual:svg-icons-register';
 import 'lazysizes';
-import uiAccordion from '../blocks/_ui/ui-accordion/ui-accordion';
-import uiDatepicker from '../blocks/_ui/ui-datepicker/ui-datepicker';
-import uiTextarea from '../blocks/_ui/ui-textarea/ui-textarea';
-import uiTooltip from '../blocks/_ui/ui-tooltip/ui-tooltip';
 import gallery from '../blocks/gallery/gallery';
 import slider from '../blocks/slider/slider';
 import tabs from '../blocks/tabs/tabs';
 
-document.addEventListener('DOMContentLoaded', function () {
-  uiAccordion();
-  uiDatepicker();
-  uiTextarea();
-  uiTooltip();
-  gallery();
-  slider();
-  tabs();
-});
+document.addEventListener('DOMContentLoaded', function () {});
