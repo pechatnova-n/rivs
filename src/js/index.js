ee67ab0c884c8a18e6e6ecb8ac41849eef9304d3
Knyazew36 <knyazew.sv@gmail.com>
2023-11-12 09:39:57 +0300

    1

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,5 @@
 import '../styles/style.scss';
 import 'virtual:svg-icons-register';
 import 'lazysizes';
-import gallery from '../blocks/gallery/gallery';
-import slider from '../blocks/slider/slider';
-import tabs from '../blocks/tabs/tabs';
 
 document.addEventListener('DOMContentLoaded', function () {});
