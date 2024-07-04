7409c90e52353c838cc99f28cbca82cf48decb4d
Knyazew36 <knyazew.sv@gmail.com>
2024-07-04 14:47:40 +0300

    barba.js

--- src/js/index.js
+++ src/js/index.js
@@ -5,10 +5,18 @@ import { select } from '../blocks/select/select';
 import { accordion } from '../blocks/accordion-ui/accordion-ui';
 import { inputReset } from './components/inputReset';
 import { modals } from '../blocks/modals/modals';
+import { barbaUi } from './libs/barba';
 
 document.addEventListener('DOMContentLoaded', function () {
+  // barbaUi();
+
+  commonFunction();
+});
+
+// Функция для вызова при переходе между страницами
+export const commonFunction = () => {
   inputReset();
   select();
   accordion();
-  modals()
-});
+  modals();
+};
